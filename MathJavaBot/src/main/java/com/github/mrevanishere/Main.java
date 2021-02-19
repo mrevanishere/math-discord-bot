0726d30e472028fbf098e159d2dccf2cc011bdca
mrevanishere <mrevanishere@gmail.com>
2021-02-18 18:40:34 -0800

    discord hello world, frac calc in package

---
+++ MathJavaBot/src/main/java/com/github/mrevanishere/Main.java
@@ -0,0 +1,40 @@
+package com.github.mrevanishere;
+// Ctrl shift o to load gradle changes in IDEA
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        // Bot's token
+        String token = "ODEyMTM3NTg2MjYzOTgyMTMx.YC8YEQ.w0vNd3A66VJzQSjNxVyrkxlp1YY";
+        DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+
+        api.addMessageCreateListener(event -> {
+            String input = event.getMessageContent();
+            int n = input.length();
+            String command = getFirstWord(input);
+            if (event.getMessageContent().equalsIgnoreCase("!ping")) {
+                event.getChannel().sendMessage("Pong!");
+            }
+            if (command.equals("!calc")) {
+                event.getChannel().sendMessage("Calculating ...");
+            }
+        });
+
+        // Add listener
+        // Print Invite URL of Bot
+    }
+
+     static private String getFirstWord(String text) {
+      int index = text.indexOf(' ');
+
+      if (index > -1) { // Check if there is more than one word.
+
+        return text.substring(0, index).trim(); // Extract first word.
+
+      } else {
+
+        return text; // Text is the first word itself.
+      }
+    }
+}
